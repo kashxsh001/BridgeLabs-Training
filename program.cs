c9029047c09b92ccc44e7ced937f30ac69569f2f
Kashish Gupta <150226464+kashxsh001@users.noreply.github.com>
2026-07-21 10:07:07 +0000

    Hello World!

---
+++ program.cs
@@ -0,0 +1,12 @@
+//Console.WriteLine("Hello, World!");
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello!!!!");
+        }
+    }
+}
