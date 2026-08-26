622ba4488f06fd38703ed2170779e6d88cdc65e6
kashish <gkashish005@gmail.com>
2026-08-26 20:25:26 +0530

    Adding Solution of Length of LIS

---
+++ PracticeQuestion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static int LengthOfLIS(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+            return 0;
+
+        int[] longest = new int[nums.Length];
+        int size = 0;
+
+        foreach (int num in nums)
+        {
+            int left = 0;
+            int right = size;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (longest[mid] < num)
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+
+            longest[left] = num;
+
+            if (left == size)
+                size++;
+        }
+
+        return size;
+    }
+
+    static void Main()
+    {
+        int[] nums = { 10, 9, 2, 5, 3, 7, 101, 18 };
+
+        Console.WriteLine(LengthOfLIS(nums));
+    }
+}
