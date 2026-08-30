45b549c5499f1d845bff6c8a6b3495ccb28dab48
kashish <gkashish005@gmail.com>
2026-08-30 13:20:27 +0530

    Completing Reflection Assignment

---
+++ Reflection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+// 1. Define the Abstract Class
+public abstract class Vehicle { }
+
+// 2. Define an intermediate concrete class
+public class Car : Vehicle { }
+
+// 3. Define the target class
+public class ElectricCar : Car { }
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Get the Type object for the class you want to inspect
+//        Type targetType = typeof(ElectricCar);
+
+//        // Find its abstract base class
+//        Type abstractBase = ReflectionExt.GetAbstractBaseClass(targetType);
+        
+//        if (abstractBase != null)
+//        {
+//            Console.WriteLine($"The abstract class implemented by {targetType.Name} is: {abstractBase.Name}");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"{targetType.Name} does not inherit from an abstract class.");
+//        }
+//    }
+//}
